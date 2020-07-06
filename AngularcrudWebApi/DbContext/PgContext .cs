b72b08df04efab386a1f5047a67e8353babf396c
Черников Сергей <chernikoff80@yandex.ru>
2020-07-06 12:14:07 +0300

    Add project files.

---
+++ AngularcrudWebApi/DbContext/PgContext .cs
@@ -0,0 +1,26 @@
+﻿using AngularcrudWebApi.DBModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AngularcrudWebApi.PgContext
+{
+    public class PgContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        //public PgContext()
+        //{
+        //    Database.EnsureCreated();
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=TestBasePostgree;Username=postgres;Password=postgres");
+        }
+    }
+}
